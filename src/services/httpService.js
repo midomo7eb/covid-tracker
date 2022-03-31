d4a8f175d2b1e17d32ab7e4e1695d8431eca59c6
Ahmed <midomo7eb@hotmail.com>
2022-03-31 15:48:25 +0200

    fixin proxy

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-// axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 axios.defaults.headers.patch["x-auth-token"] = localStorage.getItem(
   "auth-token"
 );
