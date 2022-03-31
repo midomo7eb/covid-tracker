ab2adf948271339bbae90921aca29b102d88fab8
Ahmed <midomo7eb@hotmail.com>
2022-03-31 15:47:33 +0200

    fixin proxy

--- index.js
+++ index.js
@@ -16,7 +16,17 @@ connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
 const usersRouter = require("./routes/users");
-
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://secret-savannah-40959.herokuapp.com"
+  ); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use("/users", usersRouter);
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
